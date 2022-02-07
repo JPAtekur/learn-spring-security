f3abd27f70b7fba8733f3d8a439c234bb8419dde
Atekur Rahman <jp7atik@gmail.com>
2022-02-07 22:32:42 +0600

    add basic authentication

---
+++ src/main/java/com/learn/config/MySecurityConfig.java
@@ -0,0 +1,38 @@
+package com.learn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("nirob").password(this.passwordEncoder().encode("129")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("shakila").password(this.passwordEncoder().encode("456")).roles("NORMAL");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(15);
+    }
+}
