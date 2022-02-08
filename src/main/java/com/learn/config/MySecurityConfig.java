7401e1c306a7f092972daf11bd62b2adf361d035
Atekur Rahman <jp7atik@gmail.com>
2022-02-08 23:21:45 +0600

    antMatchers and form base authentication

--- src/main/java/com/learn/config/MySecurityConfig.java
+++ src/main/java/com/learn/config/MySecurityConfig.java
@@ -2,6 +2,7 @@ package com.learn.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,17 +18,20 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeHttpRequests()
+                .antMatchers("/home").hasRole("XYZ")
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+//                .httpBasic(); // for basic authentication
+                .formLogin(); // for form base authentication
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("nirob").password(this.passwordEncoder().encode("129")).roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("shakila").password(this.passwordEncoder().encode("456")).roles("NORMAL");
+        auth.inMemoryAuthentication().withUser("nirob").password(this.passwordEncoder().encode("123")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("shakila").password(this.passwordEncoder().encode("456")).roles("XYZ");
     }
 
     @Bean
--- src/main/java/com/learn/controller/UserController.java
+++ src/main/java/com/learn/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.learn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,15 @@ public class UserController {
     @GetMapping
     public String get(){
         return "Hello Spring Security";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "From Home";
+    }
+
+    @PostMapping("/insert")
+    public String insert(){
+        return "Insert";
     }
 }
